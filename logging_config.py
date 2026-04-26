d4311576759ce26a2844f84506b48fafa6afa9c4
Zidepeople <admin@zidepeople.com>
2026-04-26 19:31:19 +0100

    chore: mark zide-mobileapp submodule as dirty

--- logging_config.py
+++ logging_config.py
@@ -1,11 +1,32 @@
 import logging
-from opencensus.ext.azure.log_exporter import AzureLogHandler
+import os
+
+
+def _resolve_log_level() -> int:
+    level_name = os.getenv("LOG_LEVEL", "INFO").upper()
+    return getattr(logging, level_name, logging.INFO)
+
+
+_LOG_LEVEL = _resolve_log_level()
+
+root_logger = logging.getLogger()
+root_logger.setLevel(_LOG_LEVEL)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(_LOG_LEVEL)
 
-handler = AzureLogHandler()
-logger.addHandler(handler)
+try:
+    from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+    handler = AzureLogHandler()
+    handler.setLevel(_LOG_LEVEL)
+    logger.addHandler(handler)
+except Exception as exc:
+    if not logger.handlers:
+        fallback_handler = logging.StreamHandler()
+        fallback_handler.setLevel(_LOG_LEVEL)
+        logger.addHandler(fallback_handler)
+    logger.warning("AzureLogHandler disabled: %s", exc)
 
 def log_event(message):
     logger.info(message)
